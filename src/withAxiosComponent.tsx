cf2220913b82ba5afdad734bf60ca422d5ecc4d8
Mateus Guedes <mxteusgued@gmail.com>
2023-06-08 23:47:58 -0300

    finish axios query

---
+++ src/withAxiosComponent.tsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface Pokemons {
+  name: string;
+  url: string;
+}
+
+function WithAxiosComponent() {
+  const [pokemons, setPokemons] = useState<Pokemons[]>([]);
+
+  async function fetchPokemons() {
+    try {
+      const response = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+      );
+      const pokemonsData = response.data;
+      console.log(pokemonsData);
+
+      if (pokemonsData) setPokemons(pokemonsData.results);
+    } catch (err) {}
+  }
+
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
+  return (
+    <div className="App">
+      {pokemons.map((item) => (
+        <span key={item?.name}>teste</span>
+      ))}
+    </div>
+  );
+}
+
+export default WithAxiosComponent;
